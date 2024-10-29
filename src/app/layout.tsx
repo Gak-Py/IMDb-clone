1e2541a9e66f9c65337a328eea93e325754bae08
Gak-py <linkle.graphics@gmail.com>
2024-10-29 17:20:36 +0900

    create navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
 import Providers from '@/app/Providers';
 
 const geistSans = localFont({
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
