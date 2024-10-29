65d57cf24c2a342dd0f8ca054629632f4edb1089
Gak-py <linkle.graphics@gmail.com>
2024-10-29 16:44:04 +0900

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs. Our website is designed to provide you
+        with a comprehensive database of movies from all around the world, along
+        with the latest news, reviews, and trailers. Our movie database is
+        constantly updated with new releases, ensuring that you have access to
+        the latest and greatest in the world of cinema. You can search for
+        movies by title, director, actor, genre, or release date, making it easy
+        to find the perfect movie for any occasion.
+      </p>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs. Our website is designed to provide you
+        with a comprehensive database of movies from all around the world, along
+        with the latest news, reviews, and trailers. Our movie database is
+        constantly updated with new releases, ensuring that you have access to
+        the latest and greatest in the world of cinema. You can search for
+        movies by title, director, actor, genre, or release date, making it easy
+        to find the perfect movie for any occasion.
+      </p>
+      <p>
+        Thank you for visiting our website and we hope you enjoy your time
+        browsing through our movie database. If you have any feedback or
+        suggestions, please feel free to contact us. We are always looking for
+        ways to improve and enhance the user experience on our website. Happy
+        browsing!
+      </p>
+    </div>
+  );
+}
